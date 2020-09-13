e26e7973799160d82a38e8122fd57876af7b3dac
Sierra Wallace <sskwallace@gmail.com>
2020-09-13 00:57:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,7 @@ class Song
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
+  def self.alphabetical
+    self.self.all.sort_by{|song| song.name}
+  end
 end
