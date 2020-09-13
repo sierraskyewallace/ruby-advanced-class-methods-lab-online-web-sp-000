06cb85b371a78034b0e5e6b2552688d4c3610eda
Sierra Wallace <sskwallace@gmail.com>
2020-09-13 01:17:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,14 @@ class Song
     self.all.sort_by{|song| song.name}
   end
   def self.new_from_filename(filename)
+    data = filename.split(" - ")
+    artist_name = data[0]
+    song_name = data[1].chomp(".mp3")
     
+    song = self.new 
+    song.name = song_name
+    song.artist_name = aartist_name
+    song
+  end
   end
 end
